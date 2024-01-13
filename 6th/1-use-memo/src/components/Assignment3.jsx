395a8e22ea7d08307c8034a537046fb63b7cdc89
WiseyXD <aryan.s.nag@gmail.com>
2024-01-13 21:02:30 +0530

    learnt useMemo

--- 6th/1-use-memo/src/components/Assignment3.jsx
+++ 6th/1-use-memo/src/components/Assignment3.jsx
@@ -1,24 +1,34 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo } from "react";
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
 export const Assignment3 = () => {
     const [items, setItems] = useState([
-        { name: 'Chocolates', value: 10 },
-        { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
+        { name: "Chocolates", value: 10 },
+        { name: "Chips", value: 20 },
+        { name: "Onion", value: 30 },
+        { name: "Tomato", value: 30 },
+        { name: "Milk", value: 30 },
+
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() => {
+        let ans = 0;
+        items.map((item) => {
+            ans += item.value;
+        });
+        return ans;
+    }, [items]);
     // Your code ends here
     return (
         <div>
             <ul>
                 {items.map((item, index) => (
-                    <li key={index}>{item.name} - Price: ${item.value}</li>
+                    <li key={index}>
+                        {item.name} - Price: ${item.value}
+                    </li>
                 ))}
             </ul>
             <p>Total Value: {totalValue}</p>
